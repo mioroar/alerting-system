d7f47793623aef7debc33c01e310a57e5c3c854b
AzaZLO <MaloyMeee@yandex.ru>
2025-07-20 17:36:21 +0300

    в команде старт теперь описаны все команды

--- bot/handlers/another.py
+++ bot/handlers/another.py
@@ -3,10 +3,8 @@ from .modules.price import price_router
 from .modules.volume import volume_router
 from .modules.volume_change import volume_change_router
 
-# Создаем главный роутер
 another_router = Router()
 
-# Включаем все модульные роутеры
 another_router.include_router(price_router)
 another_router.include_router(volume_router) 
 another_router.include_router(volume_change_router)
